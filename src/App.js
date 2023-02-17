74728c1e7169185a479bcfbb0c8d24b8d7124ee3
Anton Durandin <enitefall81@gmail.com>
2023-02-17 10:07:14 -0800

    pagination

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
+import React from "react"
 function App() {
+  let query = (new URL(document.location)).searchParams
+  let c = Number(query.get("c")) || 3
+
+  console.log(c)
+  let pages = ['1', '2', '3', '4']
   return (
     <div className="App">
-
+    <DisplayLi
+    c={c}
+    pages={pages}
+    />
     </div>
   );
+}
+
+function DisplayLi({c, pages}) {
+ return <ul>
+   {pages.map((page, index) => {
+     return <li key={index}>
+       <a>{page}</a>
+     </li>
+   })}
+ </ul>
 }
 
 export default App;
